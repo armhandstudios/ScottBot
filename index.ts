6daaafa919c1e7bed48f9e05a812154d3e822572
armhandstudios <38062783+armhandstudios@users.noreply.github.com>
2022-04-10 18:01:57 -0500

    had to save changes

--- index.ts
+++ index.ts
@@ -919,11 +919,6 @@ bot.on("message", async message =>
     {
         message.react('👍').then(() => message.react('🤷').then(() => message.react('👎')));
     }
-    if(cmd === `${tradPrefix}roll`)
-    {
-        var diceRoll = Math.floor(Math.random() * (20)); 
-        message.reply(diceRoll);
-    }
     //Secret Santa
     //secretSanta start [Description]: Starts the secret santa event. Can only be started by a Mod?.
     //secretSanta about: List the description of the secret santa event.
